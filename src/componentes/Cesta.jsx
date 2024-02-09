125b35272e9139867f3af3876d746494a02cfc94
IsisTD <220126@fppiramide.com>
2024-02-09 12:37:13 +0100

    grthftrhgf

--- src/componentes/Cesta.jsx
+++ src/componentes/Cesta.jsx
@@ -1,6 +1,10 @@
 export default function Cesta({articulosCesta, setArticulosCesta, setArticulosDisponibles, articulosDisponibles}){
     const total = articulosCesta.reduce((acc, articulo) => acc + articulo.precio, 0);
-
+    const eliminarArticuloCesta=(articulo)=>{
+        const nuevaCesta=articulosCesta.filter(item=>item.codigo !== articulo.codigo);
+        setArticulosCesta(nuevaCesta);
+        setArticulosDisponibles(articulosDisponibles.map(art=>art.codigo === articulo.codigo ?{...art,unidades:art.unidades+1}:art))
+    }
 
     return(
         <>
@@ -19,7 +23,7 @@ export default function Cesta({articulosCesta, setArticulosCesta, setArticulosDi
                         <td>{articulo.nombre}</td>
                         <td>{articulo.precio}</td>
                         <td>{articulo.unidades}</td>
-                        <td><button  onChange={()=>eliminarArticuloCesta()}>Eliminar</button></td>
+                        <td><button  onClick={()=>eliminarArticuloCesta(articulo)}>Eliminar</button></td>
                     </tr>
                 )}
                 <tr>
